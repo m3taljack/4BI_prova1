ddbc8d663501d85aada9082cb3e952be4b2f99df
4b <4b@INF16>
2022-04-23 11:49:37 +0200

    versione 1

---
+++ src/Principale.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ * @author Jacopo Bocco
+ * 
+ * @version 1.0
+ *
+ */
+public class Principale 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("prova GitHub");
+
+	}
+}
